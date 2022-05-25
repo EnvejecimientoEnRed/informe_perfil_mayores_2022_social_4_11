25a821046b4328f11abc38fe55e5f91064ff62f4
Carlos Muñoz Díaz <47306703G@cchs.csic.es>
2022-05-25 17:02:08 +0200

    Cambio de repo

--- pre/js/charts/charts.js
+++ pre/js/charts/charts.js
@@ -25,7 +25,7 @@ let dictionary = {
 
 export function initChart() {
     //Lectura de datos
-    d3.csv('https://raw.githubusercontent.com/CarlosMunozDiazCSIC/informe_perfil_mayores_2022_social_4_11/main/data/piramide_estudios_censo_2011.csv', function(error,data) {
+    d3.csv('https://raw.githubusercontent.com/EnvejecimientoEnRed/informe_perfil_mayores_2022_social_4_11/main/data/piramide_estudios_censo_2011.csv', function(error,data) {
         if (error) throw error;
         
         //Pirámide con datos absolutos
